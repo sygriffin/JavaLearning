5fad9c5846a6dbd3cec398e61840ff8476580784
sygriffin <786281584@qq.com>
2018-12-14 17:17:57 +0800

    finish I/O FileStream&FileReader

--- src/excption/TestCheck.java
+++ src/excption/TestCheck.java
@@ -51,26 +51,95 @@ public class TestCheck {
         FileInputStream fis = null;
         //向文件写数据
         FileOutputStream fos = null;
-        byte [] buffer = new byte[100];
+        byte [] buffer = new byte[1024];
 
         //从文件读数据
         try {
+
             fis = new FileInputStream("/Users/songyang/Desktop/1.txt");
             fos = new FileOutputStream("/Users/songyang/Desktop/2.txt");
-            int temp = fis.read(buffer,0,buffer.length);
+
+            //返回读取的字节数
+
+
+            while (true) {
+                int temp = fis.read(buffer,0,buffer.length);
+                if (temp == -1) {
+                    break;
+                }
+                fos.write(buffer,0,temp);
+            }
+
 //            for (int i = 0; i < buffer.length; i++){
 //                System.out.println(buffer[i]);
 //                //读出来的是ASCII码 --
 //            }
 
-            fos.write(buffer,0,temp);
+
             String s = new String(buffer);
             s = s.trim();//去掉首尾空格和空字符
             System.out.println(s);
 
+
+
         } catch ( Exception e){
             System.out.println(e);
         }
+        //关闭也要捕获异常
+        finally {
+            try {
+                fis.close();
+                fos.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        }
+
+        //字符流：读写文件时，以字符为基准FileReader，FileWriter
+
+        FileReader fr = null;
+        FileWriter fw = null;
+        char bufferC [] = new char[1024];
+
+        try {
+
+            fr = new FileReader("/Users/songyang/Desktop/1.txt");
+            fw = new FileWriter("/Users/songyang/Desktop/2.txt");
+
+
+
+//            for (int i = 0; i < bufferC.length; i++){
+//                System.out.println(bufferC[i]);
+//            }
+//            fw.write(bufferC,0,temp);
+            //大文件读写
+            while (true) {
+                int temp = fr.read(bufferC,0,bufferC.length);
+                if(temp == -1){
+                    break;
+                }
+                fw.write(bufferC,0,temp);
+            }
+
+        } catch ( Exception e){
+            System.out.println(e);
+        }
+
+
+        finally {
+            try {
+                fr.close();
+                fw.close();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        }
+
+
+
+
 
 
 
