2fb545882915cc0a46165eaa37cc6e47692f1ad3
sygriffin <786281584@qq.com>
2018-12-19 15:50:02 +0800

    finish array(less one lesson about thread)

---
+++ src/Array.java
@@ -0,0 +1,51 @@
+/**
+ * Created by songyang on 2018/12/19.
+ */
+public class Array {
+
+    public static void main(String[] args) {
+
+        //数组的静态声明
+        int arr[] = {1,2,3,2,8,6};
+
+        //基本数据类型
+        //引用数据类型
+
+        arr[3] = 10;
+        System.out.println(arr.length);
+        System.out.println(arr[3]);
+
+        //
+        int arr1[] = new int[10];
+
+        //声明字符数组
+        char arrChar [] = new char [10];
+
+        //定义二维数组
+        int arr2[][] = {{1,2,3},{4,5,6},{7,8,9}};
+
+        System.out.println(arr2[0][1]);
+
+//        arr.hashCode();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
