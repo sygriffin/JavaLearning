226ca836dbe83c3b043487f1843b6eb3bed3fb9b
sygriffin <786281584@qq.com>
2018-12-19 13:57:45 +0800

    finish thread

--- src/RunnableImpl.java
+++ src/RunnableImpl.java
@@ -4,17 +4,36 @@
 public class RunnableImpl implements Runnable {
 
 
+    public int ii = 100;
+
     @Override
     public void run() {
-        for (int i = 0;i < 100; i++){
-            System.out.println("runnable --> " + i);
-            if (i == 50){
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+//        for (int i = 0;i < 100; i++){
+//            System.out.println("runnable --> " + i);
+//            if (i == 50){
+//                try {
+//                    Thread.sleep(2000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+
+        //同步锁 -- 同步代码块
+
+
+        while(true) {
+            synchronized(this) {
+                if (ii < 0) {
+                    break;
                 }
+                System.out.println(Thread.currentThread().getName() + ii);
+                ii--;
+                Thread.yield();
+
             }
+            //会出问题 --
         }
+
     }
 }
