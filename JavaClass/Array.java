6df5a1bdff47c5fcd355e5f02cff7c3ae21e636e
sygriffin <786281584@qq.com>
2019-03-04 20:33:29 +0800

    Array continued

--- JavaClass/Array.java
+++ JavaClass/Array.java
@@ -7,6 +7,14 @@ public class Array {
         
         int[] arr = new int[5];
 
+        int [] arr1 = {1,2,3,4,5};
+
+        //数组的索引类似于key-value的方式索引查找法
+        
+        
+
+        
+
 
 
     }
