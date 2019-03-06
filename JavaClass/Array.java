7cd2a13065b18011167e9b9a9a8cfdb86abc3115
sygriffin <786281584@qq.com>
2019-03-06 15:34:37 +0800

    Array

--- JavaClass/Array.java
+++ JavaClass/Array.java
@@ -11,8 +11,13 @@ public class Array {
 
         //数组的索引类似于key-value的方式索引查找法
         
-        
+        //基本数据类型传递，不改变原值，调用后会弹栈，局部变量会消失
+        //引用类型就会改变原值，访问堆块
 
+        System.out.println(args.length);
+
+        //args -- 接受键盘录入的
+        
         
 
 
---
+++ JavaClass/ArrayTool.java
@@ -0,0 +1,38 @@
+class ArrayTool {
+   
+    //1.获取元素最大值
+    public int getMax (int[] arr) {
+        
+        int maxValue = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+             if (maxValue < arr[i]) {
+                 maxValue = arr[i];
+             }   
+        }
+        return maxValue;
+
+    }
+    //数组遍历
+    public void traverse (int[] arr) {
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+
+    //数组反转
+
+    public void reverse (int[] arr) {
+        for (int i = 0; i < arr.length/2; i++) {
+            
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length-1-i] = temp;
+        }
+    } 
+
+}
