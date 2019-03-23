e6afaaff7bc81958ead2fdb42d1ebc052c3b637d
sygriffin <786281584@qq.com>
2019-03-23 18:09:30 +0800

    finish utils.date

--- JavaDetails/javaL/src/com/basic/Demo_System.java
+++ JavaDetails/javaL/src/com/basic/Demo_System.java
@@ -2,6 +2,7 @@ package com.basic;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Date;
 
 /**
  * Created by songyang on 2019/3/23.
@@ -35,6 +36,42 @@ public class Demo_System {
 
 
         //BigDecimal -- 精确存储小数
+//        BigDecimal bd1 = new BigDecimal(2.0);
+//        BigDecimal bd2 = new BigDecimal(1.1);
+
+
+        BigDecimal bd1 = new BigDecimal("2.0");
+        BigDecimal bd2 = new BigDecimal("1.1");
+
+        BigDecimal bd3 = BigDecimal.valueOf(0.23);
+
+        System.out.println(bd1.subtract(bd2));
+
+        //入参为double的构造方法计算出来的值具有一定的不确定性 - 0.1无法准确的表示double，所以通常建议优先使用String构造方法
+        //一定是double的话，先toString，或者BigDecimal的valueOf
+        //以上方法 -- 推荐
+
+        //日期类
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getTime());
+        System.out.println(System.currentTimeMillis());//均为毫秒级的
+
+        Date date1 = new Date(0);
+        System.out.println(date1);
+        //构造方法 -- 0 ，类似dateSince1970
+        //1970-01-01
+
+        //毫秒级 -- 1s
+        Date date2 = new Date();
+        date2.setTime(1000);
+        System.out.println(date2);
+
+        
+
+
+
+
 
 
 
